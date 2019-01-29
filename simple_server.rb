263cc69c1e9d7253ad3365bfa46952122116b61b
Julien Amoros <julien@aircall.io>
2019-01-29 14:59:43 +0100

    simple_server v2

--- simple_server.rb
+++ simple_server.rb
@@ -1,5 +1,31 @@
 require 'socket'
+# require 'mysql2'
+# require 'date'
 
+# # Wait for DB to be ready
+# time = Time.now
+# client = nil
+# while Time.now < time + 30 && client.nil?
+#   begin
+#     client = Mysql2::Client.new(host: 'mysql_db', username: 'root', password: 'password', database: 'my_awesome_db')
+#   rescue StandardError
+#     sleep 1
+#   end
+#
+#   next if client.nil?
+#
+#   puts 'Connection to DB successful!'
+#   client.close
+# end
+#
+# # Connect to DB, drop and create table
+# client = Mysql2::Client.new(host: 'mysql_db', username: 'root', password: 'password', database: 'my_awesome_db')
+# client.query('DROP TABLE IF EXISTS view')
+# client.query('CREATE TABLE view(view_id INT AUTO_INCREMENT,
+#                                 date DATETIME,
+#                                 PRIMARY KEY (view_id))')
+
+# default answer for the server
 app = Proc.new do
   ['200', {'Content-Type' => 'text/html'}, ["Hello world! The time is #{Time.now}"]]
 end
@@ -13,6 +39,8 @@ while session = server.accept
   status, headers, body = app.call({})
 
   session.print "HTTP/1.1 #{status}\r\n"
+
+  # client.query("INSERT INTO view(date) VALUES ('#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}')")
 
   headers.each do |key, value|
     session.print "#{key}: #{value}\r\n"
